a53941ea90df4579b62942a94fee1bbbd7fe6b43
lsx.lenovo <lsx@163.com>
2016-07-14 10:18:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Android移动应用基础教程，第6章第一个练习
+建立数据库
+自己插入数据
+创建contentprovider
+利用contentprovider读取数据库中的数据
---
+++ app/src/main/java/com/fei/contentproviderexer/PersonInfoListActivity.java
@@ -0,0 +1,13 @@
+package com.fei.contentproviderexer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonInfoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_info_list);
+    }
+}
